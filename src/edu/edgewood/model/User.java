e05670392b970d8fe54a4ee48cf43ae159f11580
Adam Gibson <adamgibs@outlook.com>
2019-04-28 12:07:59 -0500

    Added User Model

---
+++ src/edu/edgewood/model/User.java
@@ -0,0 +1,11 @@
+package edu.edgewood.model;
+
+import java.time.LocalDate;
+
+public class User {
+	String userId;
+	String password;
+	String firstName;
+	String lastName;
+	LocalDate createDate;
+}
