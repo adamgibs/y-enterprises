0355992719441e59c2e584e0e81196159b05467f
Adam Gibson <adamgibs@outlook.com>
2019-04-28 12:12:28 -0500

    Updated user model

--- src/edu/edgewood/model/User.java
+++ src/edu/edgewood/model/User.java
@@ -8,4 +8,21 @@ public class User {
 	String firstName;
 	String lastName;
 	LocalDate createDate;
+	public String getUserId() {
+		return userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public LocalDate getCreateDate() {
+		return createDate;
+	}
+	
+	
 }
