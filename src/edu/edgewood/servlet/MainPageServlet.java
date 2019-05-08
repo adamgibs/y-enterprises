414b5bf131f8e1ca030aada199d5e7d81ef31e37
Adam Gibson <adamgibs@outlook.com>
2019-05-08 08:09:18 -0500

    modified web.xml - mainPage.jsp is landing page

--- src/edu/edgewood/servlet/MainPageServlet.java
+++ src/edu/edgewood/servlet/MainPageServlet.java
@@ -12,30 +12,15 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "main", urlPatterns = { "/main" })
 public class MainPageServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MainPageServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
+   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/WEB-INF/jsp/mainPage.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
